ba24f0b86d054104333d493bad0caa9c777ade25
KSneijders <ksneijders@hotmail.com>
2023-04-16 19:12:02 +0200

    Fixed version variable comment
    copy-pasta is difficult

--- AoE2ScenarioRms/version.py
+++ AoE2ScenarioRms/version.py
@@ -1,4 +1,4 @@
 """ Expose version """
 
-__version__ = "0.2.5"  # << Also change `AoE2ScenarioRms/version.py`
+__version__ = "0.2.5"  # << Also change `/pyproject.toml`
 VERSION = __version__.split(".")
